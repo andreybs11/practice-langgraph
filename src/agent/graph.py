9ef8cabee7bf47b4001ea7ce479db7c828e4a424
Andrey Bolanos <andreybs11@gmail.com>
2025-08-03 20:37:49 -0600

    Practice: basic agent with LLM

--- src/agent/graph.py
+++ src/agent/graph.py
@@ -5,12 +5,13 @@ Returns a predefined response. Replace logic and configuration as needed.
 
 from __future__ import annotations
 
-from dataclasses import dataclass
-from typing import Any, Dict, TypedDict
+from typing import Any, Dict, TypedDict, Annotated, Sequence
 
+from langchain_core.messages import AIMessage, BaseMessage, SystemMessage
 from langchain_core.runnables import RunnableConfig
+from langchain_groq import ChatGroq
 from langgraph.graph import StateGraph
-
+from langgraph.graph.message import add_messages
 
 class Configuration(TypedDict):
     """Configurable parameters for the agent.
@@ -22,33 +23,39 @@ class Configuration(TypedDict):
     my_configurable_param: str
 
 
-@dataclass
-class State:
+class State(TypedDict):
     """Input state for the agent.
 
     Defines the initial structure of incoming data.
     See: https://langchain-ai.github.io/langgraph/concepts/low_level/#state
     """
+    # messages: List[Union[HumanMessage, AIMessage, SystemMessage]] = [SystemMessage(content="You are a Principal Software Engineer that helps with software development.")]
+    messages: Annotated[Sequence[BaseMessage], add_messages]
 
-    changeme: str = "example"
+
+llm = ChatGroq(model="llama3-8b-8192")
+# llm = ChatGroq(model="llama-3.1-405b-reasoning", temperature=0)
 
 
-async def call_model(state: State, config: RunnableConfig) -> Dict[str, Any]:
+def call_model(state: State, config: RunnableConfig) -> Dict[str, Any]:
     """Process input and returns output.
 
     Can use runtime configuration to alter behavior.
     """
     configuration = config["configurable"]
-    return {
-        "changeme": "output from call_model. "
-        f'Configured with {configuration.get("my_configurable_param")}'
-    }
+    # configuration.get("my_configurable_param")
+
+    system_prompt = SystemMessage(content=
+        "You are a Software Engineer so try to make all responses technical."
+    )
+
+    response = llm.invoke([system_prompt] + state["messages"])
+
+    return {"messages": [response]}
 
 
 # Define the graph
-graph = (
-    StateGraph(State, config_schema=Configuration)
-    .add_node(call_model)
-    .add_edge("__start__", "call_model")
-    .compile(name="Andrey Agent")
-)
+builder = StateGraph(State, config_schema=Configuration)
+builder.add_node(call_model)
+builder.set_entry_point("call_model")
+graph = builder.compile(name="Andrey Agent")
